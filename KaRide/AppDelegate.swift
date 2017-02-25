a3198f5fdb1ae538815d18f918baba246c63f19a
Elliot Licht <tikhomirovi@elliots-macbook-air.local>
2017-02-25 12:45:40 +1100

    Add Facebook Login

--- KaRide/AppDelegate.swift
+++ KaRide/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FacebookCore
+import FacebookLogin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        FIRApp.configure()
         return true
     }
 
@@ -40,6 +45,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let appId = SDKSettings.appId
+        if url.scheme != nil && url.scheme!.hasPrefix("fb\(appId)") && url.host ==  "authorize" { // facebook
+            return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+        }
+        return false
+    }
+
 
 
 }
---
+++ KaRide/LoginViewController.swift
@@ -0,0 +1,106 @@
+
+//
+//  LoginViewController.swift
+//  KaRide
+//
+//  Created by Elliot Licht on 25/02/2017.
+//  Copyright © 2017 Ilia Tikhomirov. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FacebookCore
+import FacebookLogin
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginDidTouch(_ sender: Any) {
+        
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+                print(accessToken)
+                
+                
+                let connection = GraphRequestConnection()
+                connection.add(GraphRequest(graphPath: "/me?fields=first_name")) { httpResponse, result in
+                    switch result {
+                    case .success(let response):
+                        print("Graph Request Succeeded: \(response.dictionaryValue!)")
+                        let userID: String = response.dictionaryValue!["id"] as! String
+                        let firstName: String = response.dictionaryValue!["first_name"] as! String
+                        //  self.imageOutlet.downloadedFrom(link: "https://graph.facebook.com/\(userID)/picture?type=large")
+                        
+                        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                        
+                        //                 print(AccessToken.current?.authenticationToken)
+                        
+                        let credential = FIRFacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+                        
+                        
+                        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+                            // ...
+                            if let error = error {
+                                // ...
+                                print(error)
+                                return
+                            }
+                            print("LOGIN SEQUENCE")
+                            //         print(user)
+                            
+                            if let userLoged = user {
+                             //   appDelegate.ref.child("dormmates").child(userLoged.uid).setValue(["userID": userID, "name": firstName, "photo": "https://graph.facebook.com/\(userID)/picture?type=large"])
+                                
+                              //  CurrentUser.sharedInstance.createProfile(name: firstName, imageLink: "https://graph.facebook.com/\(userID)/picture?type=large", description: "no data")
+                                
+                              //  print(CurrentUser.sharedInstance.profile)
+                                
+                                //self.performSegue(withIdentifier: "profileoverview", sender: self)
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    case .failed(let error):
+                        print("Graph Request Failed: \(error)")
+                    }
+                }
+                connection.start()
+                
+            }
+            
+        }
+
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
